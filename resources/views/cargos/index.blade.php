07f0177518309cd7bef170b9032905f195c83d07
Diego.Fast <dieggo.albuquerque@gmail.com>
2022-10-19 16:52:55 -0300

    views ajuste controllers

--- resources/views/cargos/index.blade.php
+++ resources/views/cargos/index.blade.php
@@ -16,15 +16,16 @@
             </tr>
         </thead>
         <tbody>
-            <tr>
-                <td></td>
-                <td></td>
+            @foreach ($cargos as $cargo)
+            <tr class="text-center">
+                <td class="align-middle">{{ $cargo->id }}</td>
+                <td class="align-middle">{{ $cargo->descricao }}</td>
                 <td class="align-middle"><button type="button" class="btn btn-success m-2">
                         <i class="bi bi-person"></i></button><button type="button" class="btn btn-primary m-2">
                         <i class="bi bi-pen"></i></button><button type="button" class="btn btn-danger m-2">
                         <i class="bi bi-trash3"></i></button></td>
             </tr>
-
+            @endforeach
         </tbody>
     </table>
 @endsection
